d908b7133c105ead3bf6f661d94741400c48a65c
peppidesu <bakker.pepijn@gmail.com>
2026-04-16 18:12:03 +0200

    remove fs definition

--- machines/ferry/hw.nix
+++ machines/ferry/hw.nix
@@ -5,7 +5,7 @@
 
 {
   imports =
-    [ 
+    [
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
@@ -13,11 +13,6 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-      fsType = "ext4";
-    };
 
   swapDevices = [ ];
 
